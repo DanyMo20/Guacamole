ae0bdce8a0b42842a37a324109b2c2e926e81ae3
Daniel Morawiec <danielmorawiec20@gmail.com>
2023-07-30 18:24:05 +0200

    Add config.py

---
+++ rest-api/api/config.py
@@ -0,0 +1,20 @@
+# Change this according to parameters of your Apache Guacamole. Required.
+ENDPOINT = "http://192.168.56.102:8080/guacamole"
+AUTH_USERNAME = "guacadmin"
+AUTH_PASSWORD = "guacadmin"
+DATA_SOURCE = "mysql"
+
+# Optional to change. Required only if Guacamole already contains users, connections, etc. Important to set to avoid conflicts between names.
+USERNAME = "johnexample"
+USER_GROUP_IDENTIFIER = "userGroup-example"
+MEMBER_USER_GROUP_IDENTIFIER = "memberUserGroup-example"
+PARENT_USER_GROUP_IDENTIFIER = "parentUserGroup-example"
+CONNECTION_NAME_VNC = "VNC-example"
+CONNECTION_NAME_SSH = "SSH-example"
+CONNECTION_NAME_RDP = "RDP-example"
+CONNECTION_NAME_TELNET = "Telnet-example"
+CONNECTION_NAME_KUBERNETES = "Kubernetes-example"
+CONNECTION_GROUP_NAME = "connectionGroup-example"
+SHARING_PROFILE_NAME = "sharingProfile-example"
+
+
